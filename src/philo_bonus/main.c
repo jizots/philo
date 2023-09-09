d6ae6962a479df3ac7c7146379e624da8bc6bac8
sota <hotphilly1776@yahoo.co.jp>
2023-09-09 12:41:44 +0900

    program dose not finish when eating time zero. Fixes #3 i have to die after drop off forks and cordinator!

--- src/philo_bonus/main.c
+++ src/philo_bonus/main.c
@@ -6,17 +6,29 @@
 /*   By: hotph <hotph@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:46:36 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/09/09 10:07:37 by hotph            ###   ########.fr       */
+/*   Updated: 2023/09/09 12:40:08 by hotph            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_b.h"
+
+/*May depend on environment.
+ If program was terminated by signal, opened sem data will remain memory.
+ And, if sem_post dose not complete before signal, 
+ sem_wait of new program can't seccess at all.*/
+static void	init_program(void)
+{
+	sem_unlink("forks");
+	sem_unlink("cordinator");
+	sem_unlink("print_sem");
+}
 
 int	main(int ac, char *av[])
 {
 	t_param		param;
 	int			status;
 
+	init_program();
 	status = philo_analys_argv(ac, av, &param);
 	if (status != 0)
 		return (status);
